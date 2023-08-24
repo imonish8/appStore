50e98c51af3ac19105377a8e12b290ac5990db70
Monish Nule <115737071+imonish8@users.noreply.github.com>
2023-08-24 13:03:48 +0530

    Create EmojiLoverApp.swift
    Just copy past, you can ignore this file.

---
+++ EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Monish Nule on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
