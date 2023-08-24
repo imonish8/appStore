0a407f58ca826ef85be6a72292c2a17b93232a73
Monish Nule <115737071+imonish8@users.noreply.github.com>
2023-08-24 13:05:09 +0530

    Swift Code
    Main file of What are you doing !

---
+++ ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  EmojiLover
+//
+//  Created by Monish Nule on 24/08/23.
+//
+
+import SwiftUI
+enum Emoji : String ,CaseIterable
+{
+    case 🏃,🧘🏻,💇,😪, 🧑‍💻
+}
+
+struct ContentView: View {
+    
+    @State var selection : Emoji = .🏃
+   
+        var body: some View {
+            NavigationView{
+                VStack{
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id:\.self){
+                        emoji in Text(emoji.rawValue)
+                        
+                    }
+                }
+                
+            }
+            .navigationTitle("What's up! ")
+            .padding()
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
